6a660ff1e0218d543dafc4f7447add34adf8a69c
Eapen Mani <eapenmani@gmail.com>
2023-11-08 07:35:56 +0530

    Updated the VPC script with the details

--- terraform/main_eks_cluster/main.tf
+++ terraform/main_eks_cluster/main.tf
@@ -3,42 +3,18 @@
 
 # Create the VPC and subnets using the VPC module
 module "vpc" {
-<<<<<<< Updated upstream
-  source   = "../modules/vpc"
-  vpc_cidr = "10.0.0.0/16"
-=======
   source       = "../modules/vpc"
   region       = var.region
   vpc_cidr     = var.vpc_cidr
   subnet_cidrs = var.subnet_cidrs
   project_name = var.project_name
->>>>>>> Stashed changes
 }
 # IAM Roles
-module "iam_roles" {
-  source = "../modules/iam_roles"
-}
-
-<<<<<<< Updated upstream
-# Create the EKS cluster using the EKS module
-module "eks" {
-  source       = "../modules/eks_cluster"
-  eks_cluster_role_arn = module.iam_roles.cluster_role_arn
-  #   vpc_id        = module.vpc.vpc_id
-  subnet_ids    = module.vpc.subnet_ids
-}
+# module "iam_roles" {
+#   source = "../modules/iam_roles"
+# }
 
 
-# Create the EKS node group using the EKS Node Group module
-module "eks_node_group" {
-  source          = "../modules/eks_node_group"
-  # clustername=module.eks.cluster_name
-  node_group_name = "my-node-group"
-  subnets = module.vpc.subnet_ids
-  eks_cluster_node_role_arn=module.iam_roles.cluster_node_role_arn
-  sg_id=module.vpc.node_security_group_id
-}
-=======
 # # Create the EKS cluster using the EKS module
 # module "eks" {
 #   source       = "./modules/eks"
@@ -54,6 +30,6 @@ module "eks_node_group" {
 #   cluster_name    = module.eks.cluster_name
 #   node_group_name = "my-node-group"
 # }
->>>>>>> Stashed changes
+
 
 
--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -30,32 +30,10 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_route_table" "custom_route_table" {
   vpc_id = aws_vpc.my_vpc.id
 
-<<<<<<< Updated upstream
-  route = [
-    {
-      cidr_block                 = "0.0.0.0/0"
-      gateway_id                 = aws_internet_gateway.igw.id
-      nat_gateway_id             = ""
-      carrier_gateway_id         = ""
-      destination_prefix_list_id = ""
-      egress_only_gateway_id     = ""
-      instance_id                = ""
-      ipv6_cidr_block            = ""
-      local_gateway_id           = ""
-      network_interface_id       = ""
-      transit_gateway_id         = ""
-      vpc_endpoint_id            = ""
-      vpc_peering_connection_id  = ""
-      core_network_arn = aws_internet_gateway.igw.arn
-    },
-  ]
-
-=======
   route = {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
->>>>>>> Stashed changes
   tags = {
     Name = "${var.project_name}-route-table"
   }
